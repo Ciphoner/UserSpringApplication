8246dfe814aab9548d22d93964d9a94eabad0f61
ciphoner <djefriednorvan@gmail.com>
2024-08-13 15:40:02 +0100

    implementation des controllers

--- src/main/java/com/example/demo/controllers/userController.java
+++ src/main/java/com/example/demo/controllers/userController.java
@@ -2,28 +2,56 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.entities.User;
 import com.example.demo.models.UserDto;
+import com.example.demo.services.UserService;
 
-public class userController {
+@RestController
+@RequestMapping("/users")
+public class UserController {
 
- public User create(UserDto user) {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody UserDto userDto) {
+
         return null;
     }
-    public List <User> findByNom(String nom) {
+
+    @GetMapping("/searchNomuserRepository.findByNom(nom);/{nom}")
+    public List<User> findByNom(@PathVariable("nom")  String nom) {
+        return userService.findByNom(nom);
+    }
+
+    @GetMapping("/searchAge/{age}")
+    public List<User> findByAge(@PathVariable("age") Integer age) {
+        return userService.findByAge(age);
+
+    }
+
+
+    @GetMapping("/list")
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+
+    public List<User> getAllMinerGirls() {
         return null;
     }
-    public List <User> findByAge(Integer age) {
-        return null;
-       
-    }
-    public List <User> getAll() {
-        return null;
-    }
-    public List <User> getAllMinerGirls() {
-        return null;
-    }
-    public List <User> getAllBoys() {
+
+    public List<User> getAllBoys() {
         return null;
     }
 
--- src/main/java/com/example/demo/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/services/impl/UserServiceImpl.java
@@ -1,46 +1,59 @@
 package com.example.demo.services.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.User;
+import com.example.demo.enums.SexeEnum;
 import com.example.demo.models.UserDto;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.services.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
+
    UserRepository userRepository;
-
-
+   
 
     public UserServiceImpl(UserRepository userRepository){
     this.userRepository = userRepository;
+    
 }
 
-
+    @Override
     public User create(UserDto userDto) {
             if(userRepository.findByEmail(userDto.getEmail())){
                 throw new RuntimeException("Email already exists");
             }
             return userRepository.save(userDto);
     }
+
+    @Override
     public List <User> findByNom(String nom) {
-        return null;
+        return userRepository.findByNom(nom);
     }
+    
+    @Override
     public List <User> findByAge(Integer age) {
-        return null;
+        return userRepository.findByAge(age);
        
     }
+
+    @Override
     public List <User> getAll() {
-        return null;
+        return userRepository.findAll();
     }
+
+    @Override
     public List <User> getAllMinerGirls() {
-        return null;
+        return userRepository.findByAgeUnderEighteen();
     }
+
+    @Override
     public List <User> getAllBoys() {
-        return null;
+        return userRepository.findAllBoy();
     }
    
 
